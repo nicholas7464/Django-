0e468de63e18d7711107da077c3f46c7013a0d02
Nichalos <122895793@qq.com>
2019-01-16 11:32:06 +0800

    数据库配置

---
+++ README.md
@@ -0,0 +1,9 @@
+django注册登录
+===
+
+## 步骤
+1.新建项目 django-admin startproject my_login
+2. 新建login模块 python manage.py startapp login
+3. 迁移数据库 python manage.py makemigrations, python manage.py migrate
+4. 运行 python manage.py runserver 127.0.0.1:8000
+
--- my_login/settings.py
+++ my_login/settings.py
@@ -74,10 +74,19 @@ WSGI_APPLICATION = 'my_login.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
+
+    'default':{
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'login',
+        'USER': 'root',
+        'PASSWORD': 'zhangkai',
+        'HOST': '127.0.0.1',
+        'PORT': '3306'
+        }
 }
 
 
@@ -103,9 +112,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
